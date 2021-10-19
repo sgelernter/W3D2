9b7d930da379232c729c91183e5e23bd23ff00e0
Jasonchu94 <Jasonchu7272@gmail.com>
2021-10-19 11:47:00 -0400

    update

--- Matching_game/card.rb
+++ Matching_game/card.rb
@@ -1,4 +1,33 @@
 class Card
+    attr_reader :value, :face_up, :card_back
+    def initialize
+        @value = rand(1..10)
+        @face_up = false
+        @card_back = "_"
+    end
 
+    def hide
+        @face_up = false
+    end
 
+    def reveal
+        @face_up = true
+    end
+
+    def to_s
+
+    end
+
+    def ==(other_card)
+        self.value == other_card.value
+    end
+
+    def print_card
+        if @face_up
+            print self.value
+        else
+            print self.card_back
+        end
+        true
+    end
 end
